3fe1656c8d495fe95a718998e7a24f578577a6bf
MostafaGalal1 <mostafag649.mg@gmail.com>
2022-12-10 19:55:45 +0200

    update_slider

--- src/app/Components/toolbar/toolbar.component.ts
+++ src/app/Components/toolbar/toolbar.component.ts
@@ -9,9 +9,9 @@ import {DataService} from "../../Services/data.service";
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
-  protected color: string = "#FFFFFF";
+  protected strokeColor: string = "#000000";
   protected fillColor: string = "transparent";
-  protected thickness: string = "5";
+  protected strokeWidth: string = "5";
 
   private data: DataService;
 
@@ -22,24 +22,67 @@ export class ToolbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  save() {
+  selectedFile!: File;
+  newFile!: FileSystemFileHandle;
+  pickerOpts: FilePickerOptions = {
+    types: [
+      {
+        description: 'Paints',
+        accept: {
+          'paints/*': ['.JSON', '.xml'],
+        },
+      },
+    ],
+    excludeAcceptAllOption: true,
+  };
+
+  async load() {
+    let stage:Konva.Circle;
+    let fileHandle: FileSystemFileHandle;
+    [fileHandle] = await window.showOpenFilePicker(this.pickerOpts);
+    console.log(fileHandle);
+    if (fileHandle.kind === 'file') {
+      this.selectedFile = await fileHandle.getFile();
+      console.log(JSON.stringify(this.selectedFile));
+      fetch('./assets/p.JSON')
+        .then((response) => response.json())
+        .then((json) => {stage = Konva.Node.create(json)
+        console.log(stage);});
+    }
   }
 
-  load() {
+  private stage:Konva.Circle = new Konva.Circle({x:43, y:43, fill:'red', stroke:'black', strokeWidth:12, draggable:true})
+  async save() {
+    var blob = new Blob([this.stage.toJSON()], { type: 'text/plain' });
+    this.selectedFile = new File([blob], "", {type: "text/plain"});
+    this.newFile = await window.showSaveFilePicker({
+      types: [
+        {
+          description: 'Paints',
+          accept: {
+            'paints/*': ['.JSON', '.xml'],
+          },
+        },
+      ],
+      excludeAcceptAllOption: true,
+      suggestedName: 'paint.JSON',
+    });
+    const writableStream = await this.newFile.createWritable();
+    await writableStream.write(this.selectedFile);
+    await writableStream.close();
   }
 
-  pickColor() {
-    this.data.setColor(this.color);
+  pickStrokeColor() {
+    this.data.setStrokeColor(this.strokeColor);
   }
 
   pickFillColor() {
     this.data.setFillColor(this.fillColor);
   }
 
-  pickThickness(event : any) {
-      this.thickness = event.target.value;
-      this.data.setThickness(this.thickness);
+  pickStrokeWidth(event : any) {
+      this.strokeWidth = event.target.value;
+      this.data.setStrokeColor(this.strokeWidth);
   }
-
 }
 
