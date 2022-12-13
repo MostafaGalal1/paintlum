1b77cba07dcace9e9365cd88f671556f7d07871f
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2022-12-13 02:58:57 +0200

    changes

--- Backend/backend/src/main/java/com/paint/backend/Service/Database.java
+++ Backend/backend/src/main/java/com/paint/backend/Service/Database.java
@@ -1,38 +1,37 @@
 package com.paint.backend.Service;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.paint.backend.Shapes.IShape;
-import org.json.JSONObject;
-import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileWriter;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.*;
 
 
-@Service
-public class Database {
+
+public final class Database {
 
     private int MaxID = 0;
     private Stack<String> UndoStack = new Stack<>();
     private Stack<String> RedoStack = new Stack<>();
     private Map<String, IShape> Shapes = new HashMap<>();
+    private static Database instance;
 
-    public String add(String ShapeData) {
+    public static Database getInstance() throws IOException {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public String add(IShape shape) {
         String ID = Integer.toString(MaxID++);
-        JSONObject jsonShape = new JSONObject(ShapeData);
-        IShape shape = ShapesFactory.create(jsonShape);
-        shape.setType((String) jsonShape.get("className"));
         shape.setID(ID);
-
         UndoStack.push(ID);
         Shapes.put(ID, shape);
         clearRedo();
 
         System.out.println(shape.draw().toString());
-
         return ID;
     }
 
@@ -48,40 +47,12 @@ public class Database {
         }
     }
 
-    private JSONObject dataCompare(JSONObject oldShape,JSONObject updatedShape){
-        JSONObject update = new JSONObject().put("attrs","");
-        for (Iterator<String> it = ((JSONObject)oldShape.get("attrs")).keys(); it.hasNext();) {
-            String key =it.next();
-            Object old= ((JSONObject)oldShape.get("attrs")).get(key);
-            Object updated = ((JSONObject)updatedShape.get("attrs")).get(key);
-
-            if(key.equals("x")||key.equals("y")||key.equals("strokeWidth")
-                    ||key.equals("radius")||key.equals("radiusX")||key.equals("radiusY")||key.equals("width")||key.equals("height")){
-                updated =  new Gson().fromJson(updated.toString(),float.class);
-            }else if(key.equals("points")){ updated =new Gson().fromJson(updated.toString(),float[].class);}
-
-            if(!old.equals(updated)){
-                if(key.equals("x")){
-                    key = "move";
-                    old = new JSONObject().put("x",old).put("y",((JSONObject)oldShape.get("attrs")).get("y"));
-                    updated = new JSONObject().put("x",updated).put("y",((JSONObject)updatedShape.get("attrs")).get("y"));
-                }else if (key.equals("y")){
-                    key = "move";
-                    old = new JSONObject().put("y",old).put("y",((JSONObject)oldShape.get("attrs")).get("x"));
-                    updated = new JSONObject().put("y",updated).put("x",((JSONObject)updatedShape.get("attrs")).get("x"));
-                }
-                return update.put("attrs",key).put("old",old).put("new",updated);
-            }
-        }
-        return update;
+    public IShape getShape(String ID){
+        return Shapes.get(ID);
     }
 
-    public void update(String updatedShape){
-        JSONObject jsonUpdatedShape = new JSONObject(updatedShape);
-        String ID = ((JSONObject)jsonUpdatedShape.get("attrs")).getString("id");
+    public void update(String ID,JSONObject update){
         UndoStack.push(ID);
-
-        JSONObject update = dataCompare(Shapes.get(ID).draw(),jsonUpdatedShape);
         Shapes.get(ID).update(update,"new");
         System.out.println(Shapes.get(ID).draw().toString());
         clearRedo();
@@ -119,31 +90,11 @@ public class Database {
         }
         System.out.println(Shapes.get(ID).draw().toString());
 
-
         return new JSONObject().put(state,Shapes.get(ID).draw()).toString();
     }
 
-    public void save(){
-        File file = new File("goda.json");
-        try {
-            FileWriter writer =new FileWriter(file);
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            gson.toJson(MaxID,writer);
-            gson.toJson(UndoStack.toString(),writer);
-            gson.toJson(RedoStack.toString(),writer);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    public String load(StringBuilder sb){
-        Gson g = new Gson();
-        return g.toJson(sb.toString());
-    }
-
-    public void restart() {
+    public void clear() {
         MaxID = 0;
         UndoStack.clear();
         RedoStack.clear();
